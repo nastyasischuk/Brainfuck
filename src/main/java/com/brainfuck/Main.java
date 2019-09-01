a3e512aa8cc5ab0c7aeb248e97148e6deac51e94
Nastya <nastyasischuk@gmail.com>
2019-09-01 16:13:26 +0300

    delete

--- src/main/java/com/brainfuck/Main.java
+++ src/main/java/com/brainfuck/Main.java
@@ -8,15 +8,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//String command="++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
-
         Interpreter interpreter = new Interpreter(inputCommand());
         interpreter.run();
         System.out.println(Cells.getInstance().getCurrentElement());
         System.out.println("Result :" + Combiner.getInstance().getResult());
 
     }
-    public static String inputCommand(){
+    private static String inputCommand(){
         System.out.println("Input command in com.brainfuck ");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
